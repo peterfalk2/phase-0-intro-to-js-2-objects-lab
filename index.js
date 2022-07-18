1ab37be107a9b7705970e1a84a280a29ce4968e8
peterfalk2 <106140224+peterfalk2@users.noreply.github.com>
2022-07-18 17:18:24 +1000

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+    name: 'value 1', 
+    streetAddress: 'value 2'
+    }
+
+
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+  
+    newObj[key] = value;
+  
+    return newObj;
+    }
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value; 
+  
+    return obj;
+  }
+
+  function deleteFromEmployeeByKey(obj, key) {
+    const newObj = { ...obj }
+   newObj[key] = undefined;
+    return newObj;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    const newObj = { ...obj }
+    delete obj[key];
+    return obj;
+    
+  }
